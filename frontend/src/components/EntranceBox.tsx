8f0d3614c1f1130157b7a059f2a100ae7b65878e
Thiago Ferraco de Andrade <thiago_ferrasso@ufrj.br>
2026-02-05 20:02:48 -0300

    feature: Logo principal + svgs
    Foi feito a logo principal do site + algumas svgs na pasta icons

--- frontend/src/components/EntranceBox.tsx
+++ frontend/src/components/EntranceBox.tsx
@@ -1,7 +1,23 @@
 export function EntranceBox() {
     return (
         <>
-            
+            <div className=" max-w-[448px] h-[748px] md:h-[704px] flex flex-col items-center gap-[32px]">
+                <div className="w-full flex flex-col items-center justify-start gap-[8px] ">
+                    <div>
+                        <div className="flex gap-[8px] items-center justify-center">
+                            <img src="src/assets/icons/styleLogo.svg" alt="Logo" />
+                                
+                            
+                            <div className="flex">
+                                <span className="text-black font-bold;
+]">
+                                    STYLE
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </>
     )
