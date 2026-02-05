26b6597a48edb3de82477621f39e69dc32d2aece
Thiago Ferraco de Andrade <thiago_ferrasso@ufrj.br>
2026-02-05 19:55:57 -0300

    config: preparando o terreno

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'
-
+import { EntranceBox } from './components/EntranceBox'
+import { SignIn } from './pages/signIn'
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-
+        <SignIn />
       </BrowserRouter>
     </>
   )
---
+++ frontend/src/pages/signIn.tsx
@@ -0,0 +1,12 @@
+import { EntranceBox } from "../components/EntranceBox";
+
+export function SignIn() {
+    return (
+        <>  
+            <div className="w-full mt-[48px] pr-[16px] pl-[16px] mb-[48px] flex items-center justify-center">
+                <EntranceBox />
+
+            </div>
+        </>
+    )
+}
