6d446fcef1c9ee23c64a4a1835aeac194c9e603d
Thiago Ferraco de Andrade <thiago_ferrasso@ufrj.br>
2026-02-06 19:04:41 -0300

    config: Importação da SingIn
    Foi feita a importação dos arquivos da página SignIn e feita as modificações para o preparo do terreno da nova página.

---
+++ frontend/src/pages/signUp.tsx
@@ -0,0 +1,12 @@
+import { EntranceBox } from "../components/EntranceBox";
+
+export function SignUp() {
+    return (
+        <>  
+            <div className="w-full mt-[48px] pr-[16px] pl-[16px] mb-[48px] flex items-center justify-center bg-gradient-to-br from-[rgba(243,244,246,0.3)] to-[rgba(243,244,246,0.1)]">
+                <EntranceBox />
+
+            </div>
+        </>
+    )
+}
