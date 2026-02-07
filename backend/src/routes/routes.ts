a6eeccf11cd7017d44a9d6692fc70496880127ee
oryandias <ryandiasamancio45@gmail.com>
2026-02-07 12:27:19 -0300

    preparing routes to test API

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -3,25 +3,27 @@ import { productController } from "../controllers/productController";
 import { variantController } from "../controllers/variant.Controller";
 import { reviewController } from "../controllers/reviewController";
 
-const router = Router();
+export const router = Router();
 
 // Rota do produto
 router.post("/product", productController.createProduct);
 router.get("/products", productController.readAllProduct);
 router.get("/product/:id", productController.readProduct);
-router.post("/product/:id", productController.updateProduct);
-router.post("/product/:id", productController.deleteProduct);
+router.put("/product/:id", productController.updateProduct);
+router.delete("/product/:id", productController.deleteProduct);
 
 // Rota da variante
 router.post("/variant/:productId", variantController.createVariant);
-router.post("/variants/:productId", variantController.readAllVariant);
-router.post("/variant/:id", variantController.readVariant);
-router.post("/variant/:id", variantController.updateVariant);
-router.post("/variant/:id", variantController.deleteVariant);
+router.get("/variants/:productId", variantController.readAllVariant);
+router.get("/variant/:id", variantController.readVariant);
+router.put("/variant/:id", variantController.updateVariant);
+router.delete("/variant/:id", variantController.deleteVariant);
 
 // Rota da variante
 router.post("/review/:productId", reviewController.createReview);
-router.post("/reviews/:productId", reviewController.readAllReview);
-router.post("/review/:id", reviewController.readReview);
-router.post("/review/:id", reviewController.updateReview);
-router.post("/review/:id", reviewController.deleteReview);
+router.get("/reviews/:productId", reviewController.readAllReview);
+router.get("/review/:id", reviewController.readReview);
+router.put("/review/:id", reviewController.updateReview);
+router.delete("/review/:id", reviewController.deleteReview);
+
+// export router;
