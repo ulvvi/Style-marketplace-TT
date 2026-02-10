d2707bd550bc846ee54f5fa1116815431f85cd3e
Thiago Ferraco de Andrade <thiago_ferrasso@ufrj.br>
2026-02-10 20:50:27 -0300

    .

--- frontend/src/pages/megaSale.tsx
+++ frontend/src/pages/megaSale.tsx
@@ -1,8 +1,27 @@
 export function MegaSale() {
     return (
         <>  
-           
+           <div className="bg-linear-to-r ">
+                
+           </div>
         </>
+        /* Section */
+
+/* Layout automático */
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+padding: 80px 260px;
+isolation: isolate;
+
+position: absolute;
+height: 398px;
+left: 0px;
+right: 0px;
+top: 0px;
+
+background: linear-gradient(90deg, #EF4444 0%, #DB2777 100%);
+
     )
 }
 
