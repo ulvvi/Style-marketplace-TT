9f97d09fdd252dae111c42112ba0fd7c44a64124
Thiago Ferraco de Andrade <thiago_ferrasso@ufrj.br>
2026-02-11 18:50:49 -0300

    refactor: Correção dos nomes

--- frontend/src/components/profile/PrincipalContainer.tsx
+++ frontend/src/components/profile/PrincipalContainer.tsx
@@ -1,15 +1,17 @@
+import { PersonCardName } from "./PersonCardName";
+import { ProfileContainer } from "./ProfileContainer";
 import { TabList } from "./TabList";
 
-export function PrincipalContainer (){
+export function PrincipalContainer(){
     return(
         <>
-        <div className="w-full gap-[8px]">
-        <TabList/>
-        
-        </div>
-        
-        
-        
+            <div className="flex flex-col items-center justify-start gap-[32px] mt-[32px] pr-[16px] mb-[32px] pl-[16px]">
+            <PersonCardName name={"Jhon Doe"} email={"john.doe@example.com"}/>
+            <PrincipalContainer/>
+            
+
+            </div>
         </>
     )
+
 }
