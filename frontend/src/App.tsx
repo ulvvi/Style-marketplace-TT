591286237b559d8b3b6b15c6bd687d9c09917bf3
Thiago Ferraco de Andrade <thiago_ferrasso@ufrj.br>
2026-02-11 18:40:01 -0300

    feature: ContentBox COMPONENT

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import { BrowserRouter } from "react-router"
 import { Home } from './pages/Home'
 import './App.css'
+import { Profile } from './pages/Profile'
 
 function App() {
   return (
     <>
     <BrowserRouter>
-      <Home/>
+      <Profile/>
     </BrowserRouter>
     </>
   )
---
+++ frontend/src/components/profile/ContentBox.tsx
@@ -0,0 +1,12 @@
+import { Children } from 'react';
+
+export function ContentBox({ children }){
+
+    return(
+        <>
+        
+        
+        
+        </>
+    )
+}
