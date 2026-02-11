15a31493a24e3d3eb2b42c088e82393b4b278b3c
Thiago Ferraco de Andrade <thiago_ferrasso@ufrj.br>
2026-02-11 20:46:21 -0300

    refactor: Adição do ContentBox

--- frontend/src/components/profile/ProfileContainer.tsx
+++ frontend/src/components/profile/ProfileContainer.tsx
@@ -1,11 +1,14 @@
 import { TabList } from "./TabList";
+import { ContentBox } from "./ContentBox";
 
 export function ProfileContainer (){
     return(
         <>
         <div className="w-full gap-[8px]">
         <TabList/>
-        
+        <ContentBox>
+            
+        </ContentBox>
         </div>
         
         
