2bc1f08a21adf7692b0e16dae668db02347badf7
Taylor <taylorhorta@gmail.com>
2026-02-11 23:51:00 -0300

    bugfix: componente de checkbox agora é genérico

--- frontend/src/components/Checkbox.tsx
+++ frontend/src/components/Checkbox.tsx
@@ -1,11 +1,18 @@
-export function CheckBox(){
-    return(
-        <>
+import type { ReactNode } from 'react';
+interface CheckboxProps{
+    label: ReactNode
+}
+
+export function Checkbox({label}: CheckboxProps){
+    <label className="flex items-center space-x-2">
+        <div className="relative flex items-center justify-center">
             <input type="checkbox" className="peer appearance-none h-[16px] w-[16px] border-2 border-primary rounded-full checked:bg-primary transition-all cursor-pointer" />
+            
             <svg className="absolute w-4 h-4 text-white pointer-events-none opacity-0 peer-checked:opacity-100 transition-opacity" 
                 xmlns="http://www.w3.org" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-linejoin="round">
             <polyline points="20 6 9 17 4 12"></polyline>
             </svg>
-        </>
-    )
+        </div>
+        <span className="ml-[8px] text-[14px] text-primary font-semibold">{label}</span>
+    </label>
 }
