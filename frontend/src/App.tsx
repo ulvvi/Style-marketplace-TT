d756122b12aa5db5b75371f2330bc6e6baa45ea7
Thiago Ferraco de Andrade <thiago_ferrasso@ufrj.br>
2026-02-12 21:47:58 -0300

    config: tirando duplicidade

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import { BrowserRouter } from "react-router"
 import { Home } from './pages/Home'
 import './App.css'
-import { BrowserRouter } from 'react-router-dom'
 import { EntranceBoxExtended } from './components/EntranceBoxExtended'
 import { SignUp } from './pages/signUp'
 
