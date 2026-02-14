d1a797807370d61de3accec486f8249b6094c932
Taylor <taylorhorta@gmail.com>
2026-02-13 22:37:59 -0300

    feature: product card agora pode receber um array de badge + refactor

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -1,6 +1,7 @@
 import { Button } from "./Button";
 import { SvgIconProduct } from "./SvgIconProduct";
-
+type badgeType = 'Best Seller' | 'New' | 'Sale' | 'Premium' | 'Limited Time' 
+        | 'Flash Sale' | 'Luxury Sale' | 'Summer Sale' | 'Sport Sale' | 'Out Of Stock'
 interface ProductCardProps {
     title: string;
     ratingAvg: number;
@@ -8,29 +9,60 @@ interface ProductCardProps {
     currentPrice: number;
     oldPrice?: number;
     category?: 'Tops'| 'Bottoms' | 'Dresses' | 'Shoes' | 'Acessories';
-    productBadge?: 'Best Seller' | 'New' | 'Sale' | 'Premium'
-    cardStyle?: 'Home' | 'Sales';
+    productBadge?: badgeType[]
+    
+
+    cardStyle?: 'Home' | 'Sales'| 'Wishlist';
     imgSrc?: string;
     imgAlt?: string;
 }
 
-export function ProductCard({title="product", ratingAvg=0, ratingQuantity=0, currentPrice=0, oldPrice, productBadge="New", imgSrc="/src/assets/placeholder.svg", imgAlt, cardStyle, category}:ProductCardProps) {
-    let discount;
-    if(cardStyle === 'Sales'){
-        discount = Math.round( (1 - (currentPrice/(oldPrice as number)))*100 ) 
+export function ProductCard({title="product", ratingAvg=0, ratingQuantity=0, currentPrice=0, oldPrice, productBadge=[], imgSrc="/src/assets/placeholder.svg", imgAlt, cardStyle, category}:ProductCardProps) {
+    const isHome = cardStyle === 'Home'
+    const isSales = cardStyle === 'Sales'
+    const isWishlist = cardStyle === 'Wishlist'
+
+    let allBadges:string[] = [...productBadge];
+    const priceColor: string = isSales ? "text-red-text" : "text-primary"
+    const buttonColor: "default" | "white" | "red" = cardStyle != 'Home' ? "default" : "white"
+
+    if(isSales){
+        const discount = Math.round( (1 - (currentPrice/(oldPrice as number)))*100 ) 
+        allBadges.unshift(`-${discount.toString()}%`)
+    }
+    if(allBadges.length == 0){
+        allBadges = ["New"];
+    }
+    function getBadgeColor(text:string){
+        if(text.includes('%')  || text == 'Sale'){
+            return "bg-[#EF4343] text-secondary";
+        }
+        if(text == 'Out Of Stock'){
+            return "bg-[#EF4343] text-secondary";
+        }
+        if(!isHome){
+            return "bg-[#F3F4F6] text-black";
+        }
+        return"bg-primary text-secondary";
     }
     
 
-    const badgeColor: string = productBadge==="Sale" || cardStyle === 'Sales'? "bg-[#EF4343]" : "bg-primary";
-    const priceColor: string = cardStyle === 'Sales' ? "text-red-text" : "text-primary"
-    const buttonColor: "default" | "white" | "red" = cardStyle != 'Home' ? "default" : "white"
     return (
         <>
             <article className="w-89.5 h-122.5 border-0 rounded-xl shadow-lg hover:shadow-xl relative
             lg:w-79.5 lg:h-112.5">
-                <div className={`flex items-center absolute ${badgeColor} py-0.75 px-2.75 rounded-[100rem] h-5.5 top-3 left-3 z-1`}>
-                    <span className="text-secondary font-semibold text-[0.75rem]">{discount === undefined ? productBadge : '-'+discount+'%'}</span>
+
+                {/**div das badges com loop */}
+                <div className={`${isSales ? 'flex justify-between w-full' : 'flex-col'}  p-3 z-1 absolute gap-2`}>
+                    {allBadges.map( (badgeText) =>{
+                        return(
+                            <div className={`relative flex items-center ${getBadgeColor(badgeText)} py-0.75 px-2.75 rounded-[100rem] h-5.5 `}>
+                                <span className=" font-semibold text-[0.75rem]">{badgeText}</span>
+                            </div>
+                        )
+                    })}
                 </div>
+
                 <div className="h-89.5 rounded-t-xl overflow-hidden lg:h-79.5">
                     <img className="w-full h-full hover:scale-105 object-cover" src={imgSrc} alt={imgAlt}></img>
                 </div>
@@ -49,7 +81,7 @@ export function ProductCard({title="product", ratingAvg=0, ratingQuantity=0, cur
                     {/*to escondendo essa div que mostra o rating caso n seja a home. pelo que eu vi so a tela de home e order
                     tem essa mesma disposicao do rating abaixo do nome, entao acho q da pra deixar assim. o estilo da tela de order,
                     inclusive, me parece o mesmo da de home*/}
-                    <div className={`text-[0.875rem] flex items-center gap-1 ${cardStyle == 'Home' ? '' : 'hidden'}`}>
+                    <div className={`text-[0.875rem] flex items-center gap-1 ${isHome ? '' : 'hidden'}`}>
                         <img src="/src/assets/icons/starIcon.svg"></img>
                         <span className="font-semibold">{ratingAvg}</span>
                         <span className="font-normal text-tertiary ml-1">({ratingQuantity})</span>
@@ -61,7 +93,7 @@ export function ProductCard({title="product", ratingAvg=0, ratingQuantity=0, cur
                         </div>
                         <div className={`${cardStyle != 'Home' ? 'w-full flex gap-2' : 'w-25'}`}> 
                             <Button texto="Add to Cart" color={buttonColor} link="" buttonClassName="h-[length:36px]"  iconSrc={cardStyle != 'Home' ? "src/assets/icons/cartIconWhite.svg" : undefined} iconPos="left"/>
-                            <SvgIconProduct path = "\src\assets\icons\heartIcon.svg" alt="Ícone para salvar na wishlist" border="true" className={`${cardStyle == 'Home' ? 'hidden' : ''}`}/>
+                            <SvgIconProduct path = "\src\assets\icons\heartIcon.svg" alt="Ícone para salvar na wishlist" border="true" className={`${isHome ? 'hidden' : ''}`}/>
                         </div>
                     </div>
                 </div>
