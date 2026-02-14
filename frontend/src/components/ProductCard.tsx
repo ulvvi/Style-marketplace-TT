49f840186111a49052764806c9471b84b97a993e
Taylor <taylorhorta@gmail.com>
2026-02-14 20:19:31 -0300

    feature: efeitos de hover

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -62,8 +62,12 @@ export function ProductCard({title="product", ratingAvg=0, ratingQuantity=0, cur
 
     return (
         <>
-            <article className={` border-0 rounded-xl shadow-lg hover:shadow-xl relative ${isSales ? 'w-full max-w-[320px] h-auto' : 'w-89.5 h-122.5 lg:w-79.5' }`}>
-
+            <article className={` border-0 rounded-xl shadow-lg hover:shadow-xl relative ${isSales ? 'w-full max-w-[320px] h-auto' : 'w-89.5 h-122.5 lg:w-79.5' }
+                group relative`}>
+                    
+                <div className="z-20 absolute left-68 top-3 invisible group-hover:visible">
+                    <SvgIconProduct color="bg-[#F3F4F6]" border="false" onClick={likeProduct} path={`${isLiked ?'src/assets/icons/heartFilled.svg' : 'src/assets/icons/heartIcon.svg' }`} alt="Ícone para salvar na wishlist" className={`${isHome ? 'hidden' : ''} `}/>
+                </div>
                 {/**div das badges com loop */}
                 <div className={`${isSales ? 'flex justify-between w-full' : 'flex-col'}  p-3 z-1 absolute gap-2`}>
                     {allBadges.map( (badgeText) =>{
--- frontend/src/components/Sales/SalesInfo.tsx
+++ frontend/src/components/Sales/SalesInfo.tsx
@@ -3,7 +3,7 @@ interface SalesInfosProps{
 }
 export function SalesInfo({text} : SalesInfosProps){
     return(
-        <div className="w-full rounded-full bg-white p-2.5">
+        <div className="w-full rounded-full bg-white p-2.5 hover:bg-[#F3F4F6CC]/80 cursor-default">
             <p className="text-center text-red-text text-[1.125rem] font-semibold">{text}</p>
         </div>
     )
