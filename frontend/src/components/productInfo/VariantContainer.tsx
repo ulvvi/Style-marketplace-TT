92ab3e4c41a6179d694407f701ee8d22c855dce7
contilou <luiz.afconti@gmail.com>
2026-02-14 23:37:16 -0300

    feature: lógica para trocar variantes

--- frontend/src/components/productInfo/VariantContainer.tsx
+++ frontend/src/components/productInfo/VariantContainer.tsx
@@ -1,36 +1,72 @@
+import { useMemo, useState } from "react";
 import { Button } from "../Button";
 import { IconButton } from "../IconButton";
 
+interface Variant {
+    id: number;
+    color: string;
+    size: string;
+    stock: number;
+    productId: number;
+}
+
 interface VariantContainerProps {
-    colors: Color[];
-
+    variants: Variant[];
 }
 
-interface Color {
-    colorName: string;
-    colorHex: string;
+const colorMap: Record<string, string> = {
+    "black": "#030711",
+    "red": "#DC2626"
 }
 
-export function VariantContainer({colors}:VariantContainerProps) {
+const sizes: string[] = ["XS", "S", "M", "L", "XL"]
+
+export function VariantContainer({variants}:VariantContainerProps) {
+    const [currentColor, setCurrentColor] = useState<string | null>(null)
+    const [currentSize, setCurrentSize] = useState<string | null>(null)
+
+    const productColors = useMemo(() => {
+        return Array.from(new Set(variants.map((variant) => variant.color)))
+    },[variants])
+
+    const isSizeOnStock = ((size: string | null) => {
+        if (!currentColor) return false;
+        return variants.some((variant) => variant.color == currentColor && variant.size === size && variant.stock > 0)
+    })
+
+    const isColorOnStock = ((color: string) => {
+        return variants.some((variant) => variant.color === color && variant.stock > 0)
+    })
+
+    const handleChangeColor = ((color: string) => {
+        setCurrentColor(color);
+        if (!isSizeOnStock(currentSize)) {
+            setCurrentSize(null);
+        }
+        
+    })
+
+    const handleChangeSize = ((size: string) => {
+        setCurrentSize(size);
+    })
+
     return (
         <>
             <div className="flex flex-col gap-6 my-6">
                 <fieldset>
                     <legend className="text-[1rem] font-semibold mb-3">Color:</legend>
                     <div className="flex gap-3">
-                        {colors.map((color, i) =>(
-                            <button className={`w-10 h-10 bg-[${color.colorHex}] rounded-full border-2 border-[#D1D5DB] cursor-pointer`}></button>
+                        {productColors.map((color) =>(
+                            <button className={`w-10 h-10 bg-[${colorMap[color]}] rounded-full border-2 border-[#D1D5DB] cursor-pointer`} onClick={() => handleChangeColor(color)} disabled={!isColorOnStock}></button>
                         ))}
                     </div>
                 </fieldset>
                 <fieldset>
                     <legend className="text-[1rem] font-semibold mb-3">Size:</legend>
                     <div className="flex gap-2 mb-3">
-                        <Button link="/" color="white" texto="XS" buttonClassName="!h-12.5" textClassName="!font-normal !text-[1rem]"/>
-                        <Button link="/" color="white" texto="S" buttonClassName="!h-12.5" textClassName="!font-normal !text-[1rem]"/>
-                        <Button link="/" color="white" texto="M" buttonClassName="!h-12.5" textClassName="!font-normal !text-[1rem]"/>
-                        <Button link="/" color="white" texto="L" buttonClassName="!h-12.5" textClassName="!font-normal !text-[1rem]"/>
-                        <Button link="/" color="white" texto="XL" buttonClassName="!h-12.5" textClassName="!font-normal !text-[1rem]"/>
+                        {sizes.map((size) => (
+                            <Button color="white" texto={size} buttonClassName="!h-12.5 disabled:cursor-default disabled:opacity-50 disabled:hover:bg-secondary" onClick={() => handleChangeSize(size)} textClassName="!font-normal !text-[1rem]" disabled={!isSizeOnStock(size)}/>
+                        ))}
                     </div>
                     <span className="text-[0.875rem] font-semibold">Size Guide</span>
                 </fieldset>
