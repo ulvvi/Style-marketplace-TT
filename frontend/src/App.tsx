4ce29cd457b2c4e8b39c81b0e6777a874314413c
Thiago Ferraco de Andrade <thiago_ferrasso@ufrj.br>
2026-02-18 14:02:31 -0300

    config: alterando as rotas
    também "conectando" o contexto

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import { Home } from './pages/Home'
 import { ProductInfo } from './pages/ProductInfo'
 import './App.css'
 import { Profile } from './pages/Profile'
+import ReactDOM from 'react-dom/client'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { EntranceBoxExtended } from './components/EntranceBoxExtended'
 import { SignUp } from './pages/signUp'
@@ -18,12 +20,22 @@ function App() {
   return (
     <>
       
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Settings />}/>
-        </Routes>
-      </BrowserRouter>
+      <GoogleOAuthProvider clientId='623900671725-l059r7q9p91hbal82ikmqd0d2lhm7bai.apps.googleusercontent.com'>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/SignIn" element={<SignIn />}/>
+            <Route path="/SignUp" element={<SignUp />}/>
+            <Route path="/SignIn" element={<Home />}/>
+            <Route path="/" element={<Home />}/>
+            <Route path="/ProductInfo" element={<ProductInfo />}/>
+            <Route path="/Profile" element={<Profile />}/>
+            <Route path="/Settings" element={<Settings />}/>
+            <Route path="/Sales" element={<Sales />}/>
+          </Routes>
+        </BrowserRouter>
+      </GoogleOAuthProvider>
+      
     </>
   )
 }
